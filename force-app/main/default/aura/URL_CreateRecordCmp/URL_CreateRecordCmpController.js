0534329e154d03d6537c6e8fcbc2978a2b72a799
hmendez1 <HMendez1@its.jnj.com>
2019-04-18 16:29:29 -0400

    Fix component not showing on second visit.

--- force-app/main/default/aura/URL_CreateRecordCmp/URL_CreateRecordCmpController.js
+++ force-app/main/default/aura/URL_CreateRecordCmp/URL_CreateRecordCmpController.js
@@ -2,5 +2,8 @@
     // this method is called when component initializes
     onInit: function( component, event, helper ) {
         helper.handleShowCreateForm( component );
+    },
+    onPageReferenceChange: function( component, event, helper ) {
+        helper.handleShowCreateForm( component );
     }
 })
--- force-app/main/default/aura/URL_CreateRecordCmp/URL_CreateRecordCmpHelper.js
+++ force-app/main/default/aura/URL_CreateRecordCmp/URL_CreateRecordCmpHelper.js
@@ -25,7 +25,7 @@
         let urlParamMap = {
             'objectname' : '',      // object whose create form to display
             'recordtypeid' : '',    // record type for new record (optional)
-            'recordid' : ''         // id of record where button was clicked
+            'returnurl' : ''         // id of record where button was clicked
         };
 
         for ( let key in pageRef.state ) {
@@ -39,13 +39,14 @@
 
         Promise.resolve()
             .then( function() {
-                if ( !$A.util.isEmpty( urlParamMap.recordid ) ) {
+                if ( !$A.util.isEmpty( urlParamMap.returnurl ) ) {
                     // workaround for not being able to customize the cancel
                     // behavior of the force:createRecord event. instead of
                     // the user seeing a blank page, instead load in the background
                     // the very record the user is viewing so when they click cancel
                     // they are still on the same record.
-                    helper.navigateToUrl( '/' + urlParamMap.recordid );
+                    //helper.navigateToUrl( '/' + urlParamMap.recordid );
+                    helper.navigateToUrl( urlParamMap.returnurl );
                     // give the page some time to load the new url
                     // otherwise we end up firing the show create form
                     // event too early and the page navigation happens
